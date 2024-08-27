a350c79a0c9968203aa1668a27e4e2e95ea80303
Darius Kumza <dkumza2c@gmail.com>
2024-08-27 18:22:55 +0300

    user nav bar added, removed unused menus

--- src/components/pages/auth/dashboard/header/Header.tsx
+++ src/components/pages/auth/dashboard/header/Header.tsx
@@ -1,96 +1,24 @@
-import { CircleUser, Menu, Package2, Search } from 'lucide-react';
+import { Search } from 'lucide-react';
 
-import { Button } from '@/components/ui/button';
-
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu';
 import { Input } from '@/components/ui/input';
-import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
-import { NavLink } from 'react-router-dom';
 import CustomSelect from '@/components/customInputs/CustomSelect';
+import { UserNav } from './UserNav';
 
 export const Header = () => {
   return (
     <header className='sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6'>
-      <nav className='hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6'>
+      <nav className='flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6'>
         <CustomSelect />
-        <NavLink
+        {/* <NavLink
           to={'/'}
           className={({ isActive }) =>
             isActive ? 'text-foreground' : 'text-muted-foreground'
           }
         >
           Overview
-        </NavLink>
-        <NavLink
-          to={'/users'}
-          className={({ isActive }) =>
-            isActive ? 'text-foreground' : 'text-muted-foreground'
-          }
-        >
-          Users
-        </NavLink>
-        <NavLink
-          to={'/categories'}
-          className={({ isActive }) =>
-            isActive ? 'text-foreground' : 'text-muted-foreground'
-          }
-        >
-          Categories
-        </NavLink>
+        </NavLink> */}
       </nav>
-      <Sheet>
-        <SheetTrigger asChild>
-          <Button variant='outline' size='icon' className='shrink-0 md:hidden'>
-            <Menu className='h-5 w-5' />
-            <span className='sr-only'>Toggle navigation menu</span>
-          </Button>
-        </SheetTrigger>
-        <SheetContent side='left'>
-          <nav className='grid gap-6 text-lg font-medium'>
-            <NavLink
-              to={'/'}
-              className='flex items-center gap-2 text-lg font-semibold'
-            >
-              <Package2 className='h-6 w-6' />
-              <span className='sr-only'>Acme Inc</span>
-            </NavLink>
-            <NavLink to={'/'} className='hover:text-foreground'>
-              Dashboard
-            </NavLink>
-            <NavLink
-              to={'/'}
-              className='text-muted-foreground hover:text-foreground'
-            >
-              Orders
-            </NavLink>
-            <NavLink
-              to={'/'}
-              className='text-muted-foreground hover:text-foreground'
-            >
-              Products
-            </NavLink>
-            <NavLink
-              to={'/'}
-              className='text-muted-foreground hover:text-foreground'
-            >
-              Customers
-            </NavLink>
-            <NavLink
-              to={'/'}
-              className='text-muted-foreground hover:text-foreground'
-            >
-              Analytics
-            </NavLink>
-          </nav>
-        </SheetContent>
-      </Sheet>
+
       <div className='flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4'>
         <form className='ml-auto flex-1 sm:flex-initial'>
           <div className='relative'>
@@ -102,22 +30,7 @@ export const Header = () => {
             />
           </div>
         </form>
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant='secondary' size='icon' className='rounded-full'>
-              <CircleUser className='h-5 w-5' />
-              <span className='sr-only'>Toggle user menu</span>
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align='end'>
-            <DropdownMenuLabel>My Account</DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>Settings</DropdownMenuItem>
-            <DropdownMenuItem>Support</DropdownMenuItem>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>Logout</DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+        <UserNav />
       </div>
     </header>
   );
---
+++ src/components/pages/auth/dashboard/header/UserNav.tsx
@@ -0,0 +1,39 @@
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { Button } from '@/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+
+export function UserNav() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant='ghost' className='relative h-8 w-8 rounded-full'>
+          <Avatar className='h-8 w-8'>
+            <AvatarImage src='/avatars/01.png' alt='@shadcn' />
+            <AvatarFallback>SC</AvatarFallback>
+          </Avatar>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className='w-40' align='end' forceMount>
+        <DropdownMenuLabel className='font-normal'>
+          <div className='flex flex-col space-y-1'>
+            <p className='text-sm font-medium leading-none'>Darius</p>
+            <p className='text-xs leading-none text-muted-foreground'>
+              darius@home.lt
+            </p>
+          </div>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>Log out</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
