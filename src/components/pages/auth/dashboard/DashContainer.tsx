a0744d431c82ab7e9d4f921b135bc2554d9c5527
Darius Kumza <dkumza2c@gmail.com>
2024-08-29 07:43:42 +0300

    added date range picker

--- src/components/pages/auth/dashboard/DashContainer.tsx
+++ src/components/pages/auth/dashboard/DashContainer.tsx
@@ -14,6 +14,8 @@ import { useEffect } from 'react';
 import { handleExpenses, handleUserExpenses } from '@/helpers/handleExpenses';
 import { useAppSelector } from '@/redux/hooks';
 import { RecentTransactions } from './overview/RecentTransactions';
+import { DateRangePicker } from '@/components/DateRangePicker';
+import { Button } from '@/components/ui/button';
 // import { CalendarDateRangePicker } from '@/components/CalendarDateRangePicker';
 // import { Button } from 'react-day-picker';
 
@@ -32,10 +34,10 @@ export function DashContainer() {
       <div className='flex-1 space-y-4 p-8 pt-6 container'>
         <div className='flex items-center justify-between space-y-2'>
           <h2 className='text-3xl font-bold tracking-tight'>Dashboard</h2>
-          {/* <div className='flex items-center space-x-2'>
-            <CalendarDateRangePicker />
-            <Button>Download</Button>
-          </div> */}
+          <div className='flex items-center space-x-2'>
+            <DateRangePicker />
+            <Button>Filter</Button>
+          </div>
         </div>
         <Tabs defaultValue='overview' className='space-y-4'>
           <DashMenu />
