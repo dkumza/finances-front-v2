8b256dbab635fa0c73e895bee068a3828a32a8a8
Darius Kumza <dkumza2c@gmail.com>
2024-08-29 23:05:07 +0300

    drawer for new transactions added

--- src/components/pages/auth/dashboard/DashContainer.tsx
+++ src/components/pages/auth/dashboard/DashContainer.tsx
@@ -3,18 +3,18 @@ import { Card, CardHeader, CardTitle } from '@/components/ui/card';
 import { Tabs, TabsContent } from '@/components/ui/tabs';
 import { DashCards } from './overview/DashCards';
 import { DashMenu } from './DashMenu';
-import { Button } from '@/components/ui/button';
 import TransactionsTable2 from './overview/TransactionsTable2';
+import { DrawerComponent } from '@/components/DrawerComponent';
 
 export function DashContainer() {
   return (
     <div className='flex min-h-screen w-full flex-col'>
       <Header />
       <div className='flex-1 space-y-4 p-8 pt-6 container'>
-        <div className='flex items-center justify-between space-y-2'>
+        <div className='flex items justify-between'>
           <h2 className='text-3xl font-bold tracking-tight'>Dashboard</h2>
           <div className='flex items-center space-x-2'>
-            <Button>New Transaction</Button>
+            <DrawerComponent title={'New Transaction'} />
           </div>
         </div>
         <Tabs defaultValue='overview' className='space-y-4'>
